2048c9b9bf61ff6985d1b93111272bb0a81a5222
ainsma01 <ainsma01@gmail.com>
2025-11-20 18:06:13 -0500

    adding data map calculation

---
+++ matt_run.py
@@ -0,0 +1,233 @@
+import datasets
+from training_helpers import TrainingDynamicsCallback
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, \
+    AutoModelForQuestionAnswering, Trainer, TrainingArguments, HfArgumentParser
+import evaluate
+from helpers import prepare_dataset_nli, prepare_train_dataset_qa, \
+    prepare_validation_dataset_qa, QuestionAnsweringTrainer, compute_accuracy
+import os
+import json
+import numpy as np
+
+NUM_PREPROCESSING_WORKERS = 2
+
+
+def main():
+    argp = HfArgumentParser(TrainingArguments)
+    # The HfArgumentParser object collects command-line arguments into an object (and provides default values for unspecified arguments).
+    # In particular, TrainingArguments has several keys that you'll need/want to specify (when you call run.py from the command line):
+    # --do_train
+    #     When included, this argument tells the script to train a model.
+    #     See docstrings for "--task" and "--dataset" for how the training dataset is selected.
+    # --do_eval
+    #     When included, this argument tells the script to evaluate the trained/loaded model on the validation split of the selected dataset.
+    # --per_device_train_batch_size <int, default=8>
+    #     This is the training batch size.
+    #     If you're running on GPU, you should try to make this as large as you can without getting CUDA out-of-memory errors.
+    #     For reference, with --max_length=128 and the default ELECTRA-small model, a batch size of 32 should fit in 4gb of GPU memory.
+    # --num_train_epochs <float, default=3.0>
+    #     How many passes to do through the training data.
+    # --output_dir <path>
+    #     Where to put the trained model checkpoint(s) and any eval predictions.
+    #     *This argument is required*.
+
+    argp.add_argument('--model', type=str,
+                      default='google/electra-small-discriminator',
+                      help="""This argument specifies the base model to fine-tune.
+        This should either be a HuggingFace model ID (see https://huggingface.co/models)
+        or a path to a saved model checkpoint (a folder containing config.json and pytorch_model.bin).""")
+    argp.add_argument('--task', type=str, choices=['nli', 'qa'], required=True,
+                      help="""This argument specifies which task to train/evaluate on.
+        Pass "nli" for natural language inference or "qa" for question answering.
+        By default, "nli" will use the SNLI dataset, and "qa" will use the SQuAD dataset.""")
+    argp.add_argument('--dataset', type=str, default=None,
+                      help="""This argument overrides the default dataset used for the specified task.""")
+    argp.add_argument('--max_length', type=int, default=128,
+                      help="""This argument limits the maximum sequence length used during training/evaluation.
+        Shorter sequence lengths need less memory and computation time, but some examples may end up getting truncated.""")
+    argp.add_argument('--max_train_samples', type=int, default=None,
+                      help='Limit the number of examples to train on.')
+    argp.add_argument('--max_eval_samples', type=int, default=None,
+                      help='Limit the number of examples to evaluate on.')
+
+    training_args, args = argp.parse_args_into_dataclasses()
+
+    dataset = datasets.load_dataset()
+    eval_split = 'validation'
+
+    # Add an ID column to the dataset
+    def add_id(ex, idx):
+        ex['id'] = idx
+        return ex
+    dataset = dataset.map(add_id, with_indices=True)
+    
+    # NLI models need to have the output label count specified (label 0 is "entailed", 1 is "neutral", and 2 is "contradiction")
+    task_kwargs = {}
+
+
+    model_class = AutoModelForQuestionAnswering
+
+    # Initialize the model and tokenizer from the specified pretrained model/checkpoint
+    model = model_class.from_pretrained(args.model, **task_kwargs)
+    # Make tensor contiguous if needed https://github.com/huggingface/transformers/issues/28293
+    if hasattr(model, 'electra'):
+        for param in model.electra.parameters():
+            if not param.is_contiguous():
+                param.data = param.data.contiguous()
+
+    tokenizer = AutoTokenizer.from_pretrained(args.model, use_fast=True)
+
+    # Tokenize the dataset while keeping track of the example IDs
+    def preproces(ex):
+        out = tokenizer(
+            ex["context"],
+            ex["question"],
+            truncation=True,
+            max_length=384,
+            stride=128,
+            return_offsets_mapping=True,
+            padding="max_length"
+        )
+
+        out["example_id"] = example["example_id"]
+        out["start_positions"] = example["answers"]["answer_start"][0]
+        out["end_positions"]   = out["start_positions"] + len(example["answers"]["text"][0])
+
+        return out
+
+    tokenized = dataset.map(preproces, batched=False)
+
+    # Select the dataset preprocessing function (these functions are defined in helpers.py)
+    prepare_train_dataset = lambda exs: prepare_train_dataset_qa(exs, tokenizer)
+    prepare_eval_dataset = lambda exs: prepare_validation_dataset_qa(exs, tokenizer)
+    
+    train_dataset = None
+    eval_dataset = None
+    train_dataset_featurized = None
+    eval_dataset_featurized = None
+    if training_args.do_train:
+        train_dataset = dataset['train']
+        if args.max_train_samples:
+            train_dataset = train_dataset.select(range(args.max_train_samples))
+        train_dataset_featurized = train_dataset.map(
+            prepare_train_dataset,
+            batched=True,
+            num_proc=NUM_PREPROCESSING_WORKERS,
+            remove_columns=train_dataset.column_names
+        )
+    if training_args.do_eval:
+        eval_dataset = dataset[eval_split]
+        if args.max_eval_samples:
+            eval_dataset = eval_dataset.select(range(args.max_eval_samples))
+        eval_dataset_featurized = eval_dataset.map(
+            prepare_eval_dataset,
+            batched=True,
+            num_proc=NUM_PREPROCESSING_WORKERS,
+            remove_columns=eval_dataset.column_names
+        )
+
+    # Select the training configuration
+    trainer_class = Trainer
+    eval_kwargs = {}
+    # If you want to use custom metrics, you should define your own "compute_metrics" function.
+    # For an example of a valid compute_metrics function, see compute_accuracy in helpers.py.
+    compute_metrics = None
+        
+    # For QA, we need to use a tweaked version of the Trainer (defined in helpers.py)
+    # to enable the question-answering specific evaluation metrics
+    trainer_class = QuestionAnsweringTrainer
+    eval_kwargs['eval_examples'] = eval_dataset
+    metric = evaluate.load('squad')   # datasets.load_metric() deprecated
+    compute_metrics = lambda eval_preds: metric.compute(
+        predictions=eval_preds.predictions, references=eval_preds.label_ids)
+    
+
+    # This function wraps the compute_metrics function, storing the model's predictions
+    # so that they can be dumped along with the computed metrics
+    eval_predictions = None
+    def compute_metrics_and_store_predictions(eval_preds):
+        nonlocal eval_predictions
+        eval_predictions = eval_preds
+        return compute_metrics(eval_preds)
+
+    # Initialize the Trainer object with the specified arguments and the model and dataset we loaded above
+    callback = TrainingDynamicsCallback()
+    trainer = trainer_class(
+        model=model,
+        args=training_args,
+        train_dataset=train_dataset_featurized,
+        eval_dataset=eval_dataset_featurized,
+        tokenizer=tokenizer,
+        compute_metrics=compute_metrics_and_store_predictions
+        callback=[callback]
+    )
+    # Train and/or evaluate
+    if training_args.do_train:
+        trainer.train()
+        trainer.save_model()
+        # If you want to customize the way the loss is computed, you should subclass Trainer and override the "compute_loss"
+        # method (see https://huggingface.co/transformers/_modules/transformers/trainer.html#Trainer.compute_loss).
+        #
+        # You can also add training hooks using Trainer.add_callback:
+        #   See https://huggingface.co/transformers/main_classes/trainer.html#transformers.Trainer.add_callback
+        #   and https://huggingface.co/transformers/main_classes/callback.html#transformers.TrainerCallback
+
+        # Compute data map coordinates
+        data_map = []
+
+        for ex_id, points in callback.dynamics.items():
+            conf = np.mean([(p["start_logit"] + p["end_logit"]) / 2 for p in points])
+            var  = np.std([(p["start_logit"] + p["end_logit"]) / 2 for p in points])
+            cor  = np.mean([p["correct"] for p in points])
+
+            data_map.append({
+                "id": ex_id,
+                "confidence": float(conf),
+                "variability": float(var),
+                "correctness": float(cor)
+            })
+        
+        # Save data map
+        with open("squad_training_dynamics.jsonl", "w") as f:
+            for entry in data_map:
+                f.write(json.dumps(entry) + "\n")
+
+    if training_args.do_eval:
+        results = trainer.evaluate(**eval_kwargs)
+
+        # To add custom metrics, you should replace the "compute_metrics" function (see comments above).
+        #
+        # If you want to change how predictions are computed, you should subclass Trainer and override the "prediction_step"
+        # method (see https://huggingface.co/transformers/_modules/transformers/trainer.html#Trainer.prediction_step).
+        # If you do this your custom prediction_step should probably start by calling super().prediction_step and modifying the
+        # values that it returns.
+
+        print('Evaluation results:')
+        print(results)
+
+        os.makedirs(training_args.output_dir, exist_ok=True)
+
+        with open(os.path.join(training_args.output_dir, 'eval_metrics.json'), encoding='utf-8', mode='w') as f:
+            json.dump(results, f)
+
+        with open(os.path.join(training_args.output_dir, 'eval_predictions.jsonl'), encoding='utf-8', mode='w') as f:
+            if args.task == 'qa':
+                predictions_by_id = {pred['id']: pred['prediction_text'] for pred in eval_predictions.predictions}
+                for example in eval_dataset:
+                    example_with_prediction = dict(example)
+                    example_with_prediction['predicted_answer'] = predictions_by_id[example['id']]
+                    f.write(json.dumps(example_with_prediction))
+                    f.write('\n')
+            else:
+                for i, example in enumerate(eval_dataset):
+                    example_with_prediction = dict(example)
+                    example_with_prediction['predicted_scores'] = eval_predictions.predictions[i].tolist()
+                    example_with_prediction['predicted_label'] = int(eval_predictions.predictions[i].argmax())
+                    f.write(json.dumps(example_with_prediction))
+                    f.write('\n')
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ training_helpers.py
@@ -0,0 +1,37 @@
+from transformers import TrainerCallback
+from collections import defaultdict
+import torch, numpy as np
+
+class TrainingDynamicsCallback(TrainerCallback):
+
+    def __init__(self):
+        self.dynamics = defaultdict(list)
+        self.epoch = 0
+
+    def on_epoch_begin(self, args, state, control, **kwargs):
+        self.epoch = int(state.epoch)
+
+    def on_prediction_step(self, args, state, control, outputs, **inputs):
+        # outputs: model predictions
+        # inputs: batch inputs including example_id
+        start_logits = outputs.start_logits.detach().cpu()
+        end_logits   = outputs.end_logits.detach().cpu()
+
+        batch_ids = inputs["example_id"].detach().cpu()
+        gold_start = inputs["start_positions"].detach().cpu()
+        gold_end   = inputs["end_positions"].detach().cpu()
+
+        for i, ex_id in enumerate(batch_ids):
+            s_logit = start_logits[i, gold_start[i]].item()
+            e_logit = end_logits[i, gold_end[i]].item()
+
+            pred_s = torch.argmax(start_logits[i]).item()
+            pred_e = torch.argmax(end_logits[i]).item()
+            correct = (pred_s == gold_start[i]) and (pred_e == gold_end[i])
+
+            self.dynamics[int(ex_id)].append({
+                "epoch": self.epoch,
+                "start_logit": s_logit,
+                "end_logit": e_logit,
+                "correct": bool(correct)
+            })
